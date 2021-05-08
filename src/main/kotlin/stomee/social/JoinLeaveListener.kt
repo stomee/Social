3870c647a2da1938c42c8b3e552b5c700d6d0024
monkeyass700 <83846530+monkeyass700@users.noreply.github.com>
2021-05-08 16:48:22 -0400

    Leave and join chat notification

---
+++ src/main/kotlin/stomee/social/JoinLeaveListener.kt
@@ -0,0 +1,20 @@
+package stomee.social
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.minestom.server.adventure.audience.Audiences
+import net.minestom.server.entity.Player
+import net.minestom.server.event.player.PlayerDisconnectEvent
+import net.minestom.server.event.player.PlayerLoginEvent
+import world.cepi.kstom.addEventCallback
+import javax.swing.text.AbstractDocument
+
+fun hook(player: Player) {
+    player.addEventCallback<PlayerLoginEvent> {
+        Audiences.all().sendMessage(Component.text("${player.username} has joined the game!", NamedTextColor.BLUE))
+    }
+
+    player.addEventCallback<PlayerDisconnectEvent> {
+        Audiences.all().sendMessage(Component.text("${player.username} has left the game!", NamedTextColor.AQUA)
+    }
+}
--- src/main/kotlin/stomee/social/Social.kt
+++ src/main/kotlin/stomee/social/Social.kt
@@ -3,12 +3,16 @@ package stomee.social
 import net.minestom.server.extensions.Extension;
 import stomee.social.message.MessageCommand
 import stomee.social.message.ReplyCommand
+import world.cepi.kstom.Manager
 import world.cepi.kstom.command.register
 import world.cepi.kstom.command.unregister
 
 class Social : Extension() {
 
     override fun initialize() {
+
+        Manager.connection.addPlayerInitialization(::hook)
+
         DiscordCommand.register()
         MessageCommand.register()
         ReplyCommand.register()
@@ -17,6 +21,9 @@ class Social : Extension() {
     }
 
     override fun terminate() {
+
+        Manager.connection.removePlayerInitialization(::hook)
+
         DiscordCommand.unregister()
         MessageCommand.unregister()
         ReplyCommand.unregister()
