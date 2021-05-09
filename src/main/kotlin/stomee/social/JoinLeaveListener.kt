331fc9cfb420b63b7c2f66435850b6805f169065
monkeyass700 <83846530+monkeyass700@users.noreply.github.com>
2021-05-08 21:39:25 -0400

    Update bug patches

--- src/main/kotlin/stomee/social/JoinLeaveListener.kt
+++ src/main/kotlin/stomee/social/JoinLeaveListener.kt
@@ -5,12 +5,12 @@ import net.kyori.adventure.text.format.NamedTextColor
 import net.minestom.server.adventure.audience.Audiences
 import net.minestom.server.entity.Player
 import net.minestom.server.event.player.PlayerDisconnectEvent
-import net.minestom.server.event.player.PlayerLoginEvent
+import net.minestom.server.event.player.PlayerSpawnEvent
 import world.cepi.kstom.addEventCallback
 import javax.swing.text.AbstractDocument
 
 fun hook(player: Player) {
-    player.addEventCallback<PlayerLoginEvent> {
+    player.addEventCallback<PlayerSpawnEvent> {
         Audiences.all().sendMessage(Component.text("${player.username} has joined the game!", NamedTextColor.BLUE))
     }
 
